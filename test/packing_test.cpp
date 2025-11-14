236e79fbe6688aa0ba1df3f080f02ae9781facf1
Simon Truscott <saitora@gmail.com>
2025-11-15 08:08:24 +1100

    Change test values

--- test/packing_test.cpp
+++ test/packing_test.cpp
@@ -24,61 +24,61 @@ TEST(packing, integral) {
   }
   {
     // NOLINTNEXTLINE(*-avoid-magic-numbers): Test value
-    std::int16_t i = 21;
+    std::int16_t i = 4660;
     std::array<unsigned char, sizeof(i)> b = {};
     pack(i, b.data());
-    ASSERT_EQ(*reinterpret_cast<std::int16_t*>(b.data()), 0X1500);
+    ASSERT_EQ(*reinterpret_cast<std::int16_t*>(b.data()), 0X3412);
     std::int16_t j = 0;
     unpack(j, b.data());
-    ASSERT_EQ(j, 21);
+    ASSERT_EQ(j, 4660);
   }
   {
     // NOLINTNEXTLINE(*-avoid-magic-numbers): Test value
-    std::uint16_t i = 21;
+    std::uint16_t i = 4660;
     std::array<unsigned char, sizeof(i)> b = {};
     pack(i, b.data());
-    ASSERT_EQ(*reinterpret_cast<std::int16_t*>(b.data()), 0X1500);
+    ASSERT_EQ(*reinterpret_cast<std::int16_t*>(b.data()), 0X3412);
     std::uint16_t j = 0;
     unpack(j, b.data());
-    ASSERT_EQ(j, 21u);
+    ASSERT_EQ(j, 4660u);
   }
   {
     // NOLINTNEXTLINE(*-avoid-magic-numbers): Test value
-    std::int32_t i = 42;
+    std::int32_t i = 305419896;
     std::array<unsigned char, sizeof(i)> b = {};
     pack(i, b.data());
-    ASSERT_EQ(*reinterpret_cast<std::int32_t*>(b.data()), 0X2A000000);
+    ASSERT_EQ(*reinterpret_cast<std::int32_t*>(b.data()), 0X78563412);
     std::int32_t j = 0;
     unpack(j, b.data());
-    ASSERT_EQ(j, 42);
+    ASSERT_EQ(j, 305419896);
   }
   {
     // NOLINTNEXTLINE(*-avoid-magic-numbers): Test value
-    std::uint32_t i = 42;
+    std::uint32_t i = 305419896;
     std::array<unsigned char, sizeof(i)> b = {};
     pack(i, b.data());
-    ASSERT_EQ(*reinterpret_cast<std::int32_t*>(b.data()), 0X2A000000);
+    ASSERT_EQ(*reinterpret_cast<std::int32_t*>(b.data()), 0X78563412);
     std::uint32_t j = 0;
     unpack(j, b.data());
-    ASSERT_EQ(j, 42u);
+    ASSERT_EQ(j, 305419896u);
   }
 }
 
 namespace {
 
 enum class E1 : char {
-  a = 'a',
-  c = 'c'
+  a = 'z',
+  c = 'a'
 };
 
 enum class E2 : std::uint16_t {
-  a = 21,
-  c = 84
+  a = 0,
+  c = 4660
 };
 
 enum class E3 : std::uint32_t {
-  a = 21,
-  c = 84
+  a = 0,
+  c = 305419896
 };
 
 } // namespace
@@ -88,7 +88,7 @@ TEST(packing, enumeration) {
     E1 i = E1::c;
     std::array<unsigned char, sizeof(i)> b = {};
     pack(i, b.data());
-    ASSERT_EQ(*reinterpret_cast<char*>(b.data()), 0X63);
+    ASSERT_EQ(*reinterpret_cast<char*>(b.data()), 0X61);
     E1 j = E1::a;
     unpack(j, b.data());
     ASSERT_EQ(j, E1::c);
@@ -97,7 +97,7 @@ TEST(packing, enumeration) {
     E2 i = E2::c;
     std::array<unsigned char, sizeof(i)> b = {};
     pack(i, b.data());
-    ASSERT_EQ(*reinterpret_cast<std::int16_t*>(b.data()), 0X5400);
+    ASSERT_EQ(*reinterpret_cast<std::int16_t*>(b.data()), 0X3412);
     E2 j = E2::a;
     unpack(j, b.data());
     ASSERT_EQ(j, E2::c);
@@ -106,7 +106,7 @@ TEST(packing, enumeration) {
     E3 i = E3::c;
     std::array<unsigned char, sizeof(i)> b = {};
     pack(i, b.data());
-    ASSERT_EQ(*reinterpret_cast<std::int32_t*>(b.data()), 0X54000000);
+    ASSERT_EQ(*reinterpret_cast<std::int32_t*>(b.data()), 0X78563412);
     E3 j = E3::a;
     unpack(j, b.data());
     ASSERT_EQ(j, E3::c);
