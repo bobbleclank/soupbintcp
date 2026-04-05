73f91ab5e21bfbf38ffcfb452e274bb750808097
Simon Truscott <saitora@gmail.com>
2026-04-05 18:16:12 +1000

    Update client example

--- example/client/main.cpp
+++ example/client/main.cpp
@@ -1,4 +1,7 @@
 #include "bc/soup/client/client.h"
+#include "bc/soup/client/connection.h"
+#include "bc/soup/client/handler.h"
+#include "bc/soup/expected.h"
 #include "bc_soup_config.h"
 #include "io_context_runner.h"
 #include "option_convert.h"
@@ -9,6 +12,7 @@
 #include <atomic>
 #include <chrono>
 #include <cstdlib>
+#include <optional>
 #include <print>
 #include <system_error>
 #include <thread>
@@ -18,10 +22,51 @@
 using namespace bc;
 using namespace std::chrono_literals;
 
-class Client {
+class Connection final : public soup::client::Connection_handler {
+public:
+  explicit Connection(soup::client::Connection* connection)
+      : connection_(connection) {
+    connection_->set_handler(*this);
+  }
+
+  void connecting(const asio::ip::tcp::endpoint& ep) override {
+    std::println("connecting: endpoint = {}:{}", ep.address().to_string(),
+                 ep.port());
+  }
+
+  void connection_failure(asio::error_code ec, const char* phase) override {
+    std::println("connection failure: error = {}:{} {}, phase = {}",
+                 ec.category().name(), ec.value(), ec.message(), phase);
+  }
+
+  void connection_success(const asio::ip::tcp::endpoint& local_ep,
+                          const asio::ip::tcp::endpoint& remote_ep) override {
+    std::println(
+        "connection success: local endpoint = {}:{}, remote endpoint = {}:{}",
+        local_ep.address().to_string(), local_ep.port(),
+        remote_ep.address().to_string(), remote_ep.port());
+  }
+
+private:
+  soup::client::Connection* connection_ = nullptr;
+};
+
+class Client final : public soup::client::Client_handler {
 public:
   explicit Client(asio::io_context& io_context)
-      : client_(io_context.get_executor()) {}
+      : client_(io_context.get_executor()) {
+    client_.set_handler(*this);
+  }
+
+  void initialize() {
+    const auto address = asio::ip::make_address("127.0.0.1");
+    const unsigned short port = 5050;
+    asio::ip::tcp::endpoint ep(address, port);
+    const auto result = client_.add_connection(ep);
+    if (!result)
+      throw std::system_error(result.error(), "add connection");
+    connection_.emplace(*result);
+  }
 
   void start() {
     if (const auto ec = client_.start())
@@ -32,6 +77,7 @@ public:
 
 private:
   soup::client::Client client_;
+  std::optional<Connection> connection_;
 };
 
 void run(int time) {
@@ -40,6 +86,7 @@ void run(int time) {
   std::atomic<bool> keep_going = true;
   io_runner.set_signal_handler([&keep_going] { keep_going = false; });
   Client client(io_context);
+  client.initialize();
 
   io_runner.start();
   std::this_thread::sleep_for(1s);
