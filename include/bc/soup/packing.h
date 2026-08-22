ca1f3ecbed22cdc29e76fabbc9df0d4b0e0486ef
Simon Truscott <saitora@gmail.com>
2026-08-22 10:34:26 +1000

    Use concepts and requires to replace std::enable_if

--- include/bc/soup/packing.h
+++ include/bc/soup/packing.h
@@ -1,6 +1,7 @@
 #ifndef INCLUDE_BC_SOUP_PACKING_H
 #define INCLUDE_BC_SOUP_PACKING_H
 
+#include <concepts>
 #include <cstdint>
 #include <cstring>
 #include <string>
@@ -11,8 +12,7 @@
 
 namespace bc::soup {
 
-template <typename Integral,
-          std::enable_if_t<std::is_integral_v<Integral>, bool> = true>
+template <std::integral Integral>
 void pack(Integral i, void* data) {
   static_assert(sizeof(i) == 1 || sizeof(i) == 2 || sizeof(i) == 4);
   if constexpr (sizeof(i) == 2)
@@ -22,8 +22,7 @@ void pack(Integral i, void* data) {
   std::memcpy(data, &i, sizeof(i));
 }
 
-template <typename Integral,
-          std::enable_if_t<std::is_integral_v<Integral>, bool> = true>
+template <std::integral Integral>
 void unpack(Integral& i, const void* data) {
   static_assert(sizeof(i) == 1 || sizeof(i) == 2 || sizeof(i) == 4);
   std::memcpy(&i, data, sizeof(i));
@@ -33,14 +32,16 @@ void unpack(Integral& i, const void* data) {
     i = static_cast<Integral>(ntohl(static_cast<std::uint32_t>(i)));
 }
 
-template <typename Enum, std::enable_if_t<std::is_enum_v<Enum>, bool> = true>
+template <typename Enum>
+  requires std::is_enum_v<Enum>
 void pack(Enum e, void* data) {
   using T = std::underlying_type_t<Enum>;
   const auto t = static_cast<T>(e);
   pack(t, data);
 }
 
-template <typename Enum, std::enable_if_t<std::is_enum_v<Enum>, bool> = true>
+template <typename Enum>
+  requires std::is_enum_v<Enum>
 void unpack(Enum& e, const void* data) {
   using T = std::underlying_type_t<Enum>;
   T t = 0;
